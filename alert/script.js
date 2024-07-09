8a62908b3e648fc1dc162a58eeb0afbc0fe5d849
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-07-09 16:57:23 +0330

    alert with js
    Write JavaScript code to show an alert when the button is clicked.

---
+++ alert/script.js
@@ -0,0 +1,8 @@
+// select button from HTML page
+let btn = document.querySelector("#btn");
+// select button for event listener
+btn.addEventListener("click", e);
+//Function for alert by clicking the button
+function e() {
+  alert("welcome to my fist project in this semester.");
+}
