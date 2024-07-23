0760e4d3de3329d96c43dff4c1e7cc0ac907be27
shayanUNK <143071027+shayanUNK@users.noreply.github.com>
2024-07-23 16:26:06 +0330

    Update app.js
    Changing the structure.

--- class-name-obj/app.js
+++ class-name-obj/app.js
@@ -1,51 +1,51 @@
 // school information
 let school = {
-  classA: {
-    student1: {
+  classA: [
+    {
       fName: "Shayan",
       lName: "Mahmoudi",
       phoneNumber: "09917509909",
       address: "Iran,Busher,Busher",
       score: "20",
     },
-    student2: {
+    {
       fName: "Amir",
       lName: "Razavi",
       phoneNumber: "09188963246",
       address: "iran,Busher,Busher",
       score: "15",
     },
-    student3: {
+    {
       fName: "Reza",
       lName: "Alavi",
       phoneNumber: "09563948606",
       address: "iran,Bushehr,Bushehr",
       score: "18.75",
     },
-  },
-  classB: {
-    student1: {
+  ],
+  classB: [
+    {
       fName: "Ali",
       lName: "Khorandi",
       phoneNumber: "09762689463",
       address: "Iran,Busher,Busher",
       score: "14.25",
     },
-    student2: {
+    {
       fName: "Saeed",
       lName: "Davoudi",
       phoneNumber: "09763498625",
       address: "iran,Busher,Busher",
       score: "19",
     },
-    student3: {
+    {
       fName: "Parham",
       lName: "Badian",
       phoneNumber: "0648796326",
       address: "iran,Bushehr,Bushehr",
       score: "20",
     },
-  },
+  ],
 };
 // console.log(school);
 console.log("____________ student class A ____________");
